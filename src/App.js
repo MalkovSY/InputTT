31ed09ee4edddc39b5cd1d22045fdaf3447050c0
Sergey <pewpewinmyhead@gmail.com>
2021-11-08 16:39:48 +0300

    rdy, without  optimization

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 function App() {
   let [value, setValue] = useState(``);
   let [changedElem, setChangedElem] = useState({day: false,month: false,year: false,hour: false,min: false,sec: false});
+  let [count, setCount] = useState(0);
 
   const setCursorIndex = (e) => {
     const indCursorPosition = e.target.selectionStart,
@@ -42,30 +43,47 @@ function App() {
   }, [value]);
 
   const date = moment(value, 'DD/MMMM/YYYY kk:mm:ss');
-
+  
   const pushedDown = (e) => {
     const key = e.keyCode;
-
+    setCursorIndex(e);
+    
     if(key === 13) {
       setValue(date.format('DD/MMMM/YYYY kk:mm:ss'));
     }
-
-    if(key === 38 && e.ctrlKey) {
-      const cicleDate = moment(value);
-      let day = cicleDate.get('date');
-      if(changedElem.day) {
-        setValue(`${day + 1}/${cicleDate.month()}/${cicleDate.year()} ${cicleDate.hour()}:${cicleDate.minute()}:${cicleDate.second()}`);
-      }
+    
+    if(key === 38 && !e.ctrlKey) {
+      if(changedElem.day) setValue(
+        (date.date() >= date.daysInMonth()) ? date.set('date', 1).format('DD/MMMM/YYYY kk:mm:ss') : date.add(1, 'days').format('DD/MMMM/YYYY kk:mm:ss'));
+      if(changedElem.month) setValue(
+        (date.month() >= 11) ? date.set('month', 0).format('DD/MMMM/YYYY kk:mm:ss') : date.add(1, 'months').format('DD/MMMM/YYYY kk:mm:ss'));
+      if(changedElem.hour) setValue(
+        (date.hour() >= 23) ? date.set('hour', 0).format('DD/MMMM/YYYY kk:mm:ss') : date.add(1, 'hours').format('DD/MMMM/YYYY kk:mm:ss'));
+      if(changedElem.min) setValue(
+        (date.minute() >= 59) ? date.set('minute', 0).format('DD/MMMM/YYYY kk:mm:ss') : date.add(1, 'minutes').format('DD/MMMM/YYYY kk:mm:ss'));
+      if(changedElem.sec) setValue(
+        (date.second() >= 59) ? date.set('second', 0).format('DD/MMMM/YYYY kk:mm:ss') : date.add(1, 'seconds').format('DD/MMMM/YYYY kk:mm:ss'));
+    } else if(key === 40 && !e.ctrlKey) {
+      if(changedElem.day) setValue(
+        (date.date() <= 1) ? date.set('date', date.daysInMonth()).format('DD/MMMM/YYYY kk:mm:ss') : date.subtract(1, 'days').format('DD/MMMM/YYYY kk:mm:ss'));
+      if(changedElem.month) setValue(
+        (date.month() <= 0) ? date.set('month', 11).format('DD/MMMM/YYYY kk:mm:ss') : date.subtract(1, 'months').format('DD/MMMM/YYYY kk:mm:ss'));
+      if(changedElem.hour) setValue(
+        (date.hour() <= 1) ? date.set('hour', 59).format('DD/MMMM/YYYY kk:mm:ss') : date.subtract(1, 'hours').format('DD/MMMM/YYYY kk:mm:ss'));
+      if(changedElem.min) setValue(
+        (date.minute() <= 1) ? date.set('minute', 59).format('DD/MMMM/YYYY kk:mm:ss') : date.subtract(1, 'minutes').format('DD/MMMM/YYYY kk:mm:ss'));
+      if(changedElem.sec) setValue(
+        (date.second() <= 1) ? date.set('second', 59).format('DD/MMMM/YYYY kk:mm:ss') : date.subtract(1, 'seconds').format('DD/MMMM/YYYY kk:mm:ss'));
     }
 
-    if(key === 38 && !e.ctrlKey) {
+    if(key === 38 && e.ctrlKey) {
       if(changedElem.day) setValue(date.add(1, 'days').format('DD/MMMM/YYYY kk:mm:ss'));
       if(changedElem.month) setValue(date.add(1, 'months').format('DD/MMMM/YYYY kk:mm:ss'));
       if(changedElem.year) setValue(date.add(1, 'years').format('DD/MMMM/YYYY kk:mm:ss'));
       if(changedElem.hour) setValue(date.add(1, 'hours').format('DD/MMMM/YYYY kk:mm:ss'));
       if(changedElem.min) setValue(date.add(1, 'minutes').format('DD/MMMM/YYYY kk:mm:ss'));
       if(changedElem.sec) setValue(date.add(1, 'seconds').format('DD/MMMM/YYYY kk:mm:ss'));
-    } else if(key === 40 && !e.ctrlKey) {
+    } else if(key === 40 && e.ctrlKey) {
         if(changedElem.day) setValue(date.subtract(1, 'days').format('DD/MMMM/YYYY kk:mm:ss'));
         if(changedElem.month) setValue(date.subtract(1, 'months').format('DD/MMMM/YYYY kk:mm:ss'));
         if(changedElem.year) setValue(date.subtract(1, 'years').format('DD/MMMM/YYYY kk:mm:ss'));
@@ -74,7 +92,7 @@ function App() {
         if(changedElem.sec) setValue(date.subtract(1, 'seconds').format('DD/MMMM/YYYY kk:mm:ss'));
     }
   }
-  
+
   return (
     <>
       <input
